276c1dd06fb13a281566dea53b800986ccae3b5f
Muahmmad-Farooq <muhammad888xyz@gmail.com>
2025-07-23 18:10:59 +0500

    updated files

--- travel-agent/travel_agent.py
+++ travel-agent/travel_agent.py
@@ -16,30 +16,25 @@ BASE_URL = os.getenv("BASE_URL")
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not set in .env")
 
-# Gemini model provider
-class GeminiModelProvider(ModelProvider):
-    def __init__(self, api_key, model_name):
-        super().__init__()
-        self.api_key = api_key
-        self.model_name = model_name
-        self.client = AsyncOpenAI(
-                api_key=api_key,
-                base_url=BASE_URL
-        )
-    def get_model(self, model_name="gemini-2.0-flash"):
-        return OpenAIChatCompletionsModel(
-                model=self.model_name,
-                openai_client=self.client
-        )
-
-model_provider = GeminiModelProvider(api_key=GEMINI_API_KEY,model_name="gemini-2.0-flash")
-
-# Model config
-config = RunConfig(model=model_provider.get_model(),model_provider=model_provider,tracing_disabled=True)
-
 # Start chainlit app
 @cl.on_chat_start
 async def start():
+
+     # Reference: https://ai.google.dev/gemini-api/docs/openai
+    external_client = AsyncOpenAI(
+        api_key=GEMINI_API_KEY,
+        base_url=BASE_URL,
+    )
+
+    model = OpenAIChatCompletionsModel(
+        model="gemini-2.0-flash", openai_client=external_client
+    )
+
+    config = RunConfig(
+        model=model,
+        model_provider=external_client, # type: ignore
+        tracing_disabled=True 
+    )
 
     @function_tool
     def get_flights(flight: str) -> dict:
@@ -111,14 +106,14 @@ async def start():
         name="Destination Agent",
         instructions=destination_prompt,
         handoff_description="Handles destination suggestions based on user interests.",
-        model=model_provider.get_model(),
+        model=model,
     )
 
     booking_agent = Agent(
         name="Booking Agent",
         instructions=booking_prompt,
         handoff_description="Handles flight and hotel bookings using mock data.",
-        model=model_provider.get_model(),
+        model=model,
         tools=[get_flights, suggest_hotels]
     )
 
@@ -126,14 +121,14 @@ async def start():
         name="Explore Agent",
         instructions=explore_prompt,
         handoff_description="Suggests local attractions, foods, and experiences at a travel destination.",
-        model=model_provider.get_model(),
+        model=model,
     )
 
     triage_agent = Agent(
         name="TravelTriageAgent",
         instructions=triage_prompt,
         handoff_description="Main controller agent that routes user requests to the correct agent.",
-        model=model_provider.get_model(),
+        model=model,
         handoffs=[destination_agent, booking_agent, explore_agent]
     )
 
